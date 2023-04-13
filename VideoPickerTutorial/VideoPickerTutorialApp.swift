bf9728fe53a818c0ee4bdb5f7cbc0b595397432d
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-04-13 22:34:56 +0300

    Initial Commit

---
+++ VideoPickerTutorial/VideoPickerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPickerTutorialApp.swift
+//  VideoPickerTutorial
+//
+//  Created by Yasin Erdemli on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct VideoPickerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
